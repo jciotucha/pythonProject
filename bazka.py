bcd2534b30663e21fd9327331f0e5105e1ee8105
ciotuja1 <jciotucha@protonmail.com>
2021-09-10 08:12:25 +0200

    Odczyt z urządzeń.

--- bazka.py
+++ bazka.py
@@ -11,63 +11,63 @@ for line in open(FILE):
 
     for match in re.finditer(re.compile("Type"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Chassis Topology"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Location"), line):
 
         print(line.strip())
 
-    for match in re.finditer(re.compile("                           "), line):
+    for match in re.finditer(re.compile("                               "), line):
 
         print(line.rstrip())
 
     for match in re.finditer(re.compile("Coordinates"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("CLLI code"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Number of slots"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Oper number of slots"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("faceplate ports"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("physical ports"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Critical LED"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Major LED"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Minor LED"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Over Temperature"), line):
 
-        print(line)
+        print(line.strip())
 
     for match in re.finditer(re.compile("Base MAC address"), line):
 
-        print(line)
+        print(line.strip())
 
 # con = sqlite3.connect('machines.db')
 # cur = con.cursor()
