e6eb25aace9f375d244495bf72601835cf65a0d4
ciotuja1 <jciotucha@protonmail.com>
2021-09-10 08:01:32 +0200

    Odczyt z urządzeń.

--- bazka.py
+++ bazka.py
@@ -1,8 +1,77 @@
 import sqlite3
+import re
 
-# FILE = 'C:/Users/ciotuja1/PycharmProjects/pythonProject/dane2.txt'
-con = sqlite3.connect('machines.db')
-cur = con.cursor()
+FILE = 'C:/Users/ciotuja1/PycharmProjects/pythonProject/dane2.txt'
+
+for line in open(FILE):
+
+    for match in re.finditer(re.compile("Name"), line):
+
+        print(line.strip())
+
+    for match in re.finditer(re.compile("Type"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Chassis Topology"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Location"), line):
+
+        print(line.strip())
+
+    for match in re.finditer(re.compile("                           "), line):
+
+        print(line.rstrip())
+
+    for match in re.finditer(re.compile("Coordinates"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("CLLI code"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Number of slots"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Oper number of slots"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("faceplate ports"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("physical ports"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Critical LED"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Major LED"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Minor LED"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Over Temperature"), line):
+
+        print(line)
+
+    for match in re.finditer(re.compile("Base MAC address"), line):
+
+        print(line)
+
+
+# con = sqlite3.connect('machines.db')
+# cur = con.cursor()
 # con.execute("""CREATE TABLE chassis
 #                             (name VARCHAR,
 #                             type VARCHAR,
@@ -19,8 +88,8 @@ cur = con.cursor()
 #                             over_temperatur_state VARCHAR,
 #                             base_mac_address VARCHAR)""")
 
-for row in cur.execute('SELECT * FROM machines ORDER BY id'):
-        print(row)
+# for row in cur.execute('SELECT * FROM machines ORDER BY id'):
+#         print(row)
 # con.commit()
 
 # with open(FILE) as file:
@@ -36,5 +105,5 @@ for row in cur.execute('SELECT * FROM machines ORDER BY id'):
 #                                                                                        (baza[i]),
 #                                                                                         (baza[i]),
 #                                                                                         (baza[i])))
-con.commit()
-con.close()
+# con.commit()
+# con.close()
